0c2d607a5190a7d9142ce1b89b0a9e1cc4c6fa08
Thom Bruce <thom@thombruce.com>
2022-12-23 12:06:10 +0000

    feat: add Tailwind CSS module

--- packages/ui/src/views/HomeView.vue
+++ packages/ui/src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
   <main>
-    <TheWelcome />
+    This is the home view.
   </main>
 </template>
